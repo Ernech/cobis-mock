349c81711299bfb2706c36bfd308db0e8b903648
Ernech <ernechvm0509@gmail.com>
2025-01-30 16:32:35 -0400

    Prueba prestamo licitado

--- src/credits/credits.service.ts
+++ src/credits/credits.service.ts
@@ -13,11 +13,19 @@ export class CreditsService {
             result:{
                 dataCredits:[
                     {
-                        creditNumber: "1425748",
-                        disbursementDate: "31/05/2024",
+                        creditNumber: "666147",
+                        disbursementDate: "31/08/2019",
                         affiliationType: "1",
-                        capitalBalance: 256850.00,
-                        creditStatus: "0",
+                        capitalBalance: 140000.00,
+                        creditStatus: "5",
+                        currency: "1"
+                    },
+                    {
+                        creditNumber: "612677",
+                        disbursementDate: "22/05/2019",
+                        affiliationType: "1",
+                        capitalBalance: 921751.73,
+                        creditStatus: "5",
                         currency: "1"
                     }
                 
